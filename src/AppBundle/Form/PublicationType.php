408bebbad83ae4a562fa0a18f03192d51c359fd7
Michael Joyce <ubermichael@gmail.com>
2016-11-04 16:36:29 -0700

    add some publication properties.

--- src/AppBundle/Form/PublicationType.php
+++ src/AppBundle/Form/PublicationType.php
@@ -9,27 +9,30 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PublicationType extends AbstractType
 {
-
     /**
      * @param FormBuilderInterface $builder
-     * @param array                $options
+     * @param array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options) {
-        $builder->add('title');
-        $builder->add('sortableTitle');
-        $builder->add('year');
-        $builder->add('notes');
-        $builder->add('category');
-        $builder->add('genres');
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {    
+        $builder->add('title');  // string     
+        $builder->add('sortableTitle');  // string     
+        $builder->add('year');  // integer     
+        $builder->add('location');  // string     
+        $builder->add('links');  // array     
+        $builder->add('notes');  // text     
+        $builder->add('category');     
+        $builder->add('genres');     
+        $builder->add('authors');         
     }
-
+    
     /**
      * @param OptionsResolver $resolver
      */
-    public function configureOptions(OptionsResolver $resolver) {
+    public function configureOptions(OptionsResolver $resolver)
+    {
         $resolver->setDefaults(array(
             'data_class' => 'AppBundle\Entity\Publication'
         ));
     }
-
 }
