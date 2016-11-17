38541495e87ecd1bd0d71e5194a7982b6cd8c493
Michael Joyce <ubermichael@gmail.com>
2016-11-17 12:18:38 -0800

    clean up deleting comments some.

--- src/FeedbackBundle/DependencyInjection/FeedbackExtension.php
+++ src/FeedbackBundle/DependencyInjection/FeedbackExtension.php
@@ -2,12 +2,17 @@
 
 namespace FeedbackBundle\DependencyInjection;
 
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class FeedbackExtension extends Extension {
 
     public function load(array $configs, ContainerBuilder $container) {
+		$loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+		$loader->load('services.yml');
+		
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
         $map = array();
