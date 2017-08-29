70dbd40d512c50c07a2c1a4d23a68f7510bf1bfe
Michael Joyce <ubermichael@gmail.com>
2017-08-29 11:32:28 -0700

    squish the bugs. squish.

--- src/AppBundle/Tests/Services/AuthorImporterBugsTest.php
+++ src/AppBundle/Tests/Services/AuthorImporterBugsTest.php
@@ -2,19 +2,12 @@
 
 namespace AppBundle\Tests\Services;
 
-use AppBundle\Entity\Alias;
-use AppBundle\Entity\Book;
-use AppBundle\Entity\Compilation;
-use AppBundle\Entity\DateYear;
 use AppBundle\Entity\Periodical;
-use AppBundle\Entity\Person;
-use AppBundle\Entity\Place;
-use AppBundle\Entity\Publication;
 use AppBundle\Services\AuthorImporter;
 use AppBundle\Tests\DataFixtures\ORM\LoadRole;
 use AppBundle\Tests\Util\BaseTestCase;
 
-class AuthorImporterTest extends BaseTestCase {
+class AuthorImporterBugsTest extends BaseTestCase {
 
     /**
      * @var AuthorImporter
@@ -33,67 +26,25 @@ class AuthorImporterTest extends BaseTestCase {
     }
     
     // Title in the cell, twice.
-//    public function testDupeData() {
-//        $row = array_fill(0, 13, '');
-//        $row[9] = 'Alberta Poetry Yearbook (1936); Alberta Poetry Yearbook (1938)';
-//        
-//        $this->importer->setCommit(true);
-//        $this->importer->importRow($row);
-//        $periodicals = $this->em->getRepository(Periodical::class)->findAll();
-//        $this->assertEquals(1, count($periodicals));
-//    }
-
-    /**
-     * @dataProvider badDatesData
-     * @param type $expected
-     * @param type $value
-     */
-    public function testBadDates($expected, $value) {
-        list($title, $date) = $this->importer->titleDate($value);
-        $this->assertEquals($expected[0], $title);
-        $this->assertEquals($expected[1], $date);
-    }
-    
-    public function badDatesData() {
-        return [
-            [['AB', null], 'AB'],
-            [['AB', '1945'], 'AB (1945)'],
-            [['AB', '1945; 1946'], 'AB (1945; 1946)'],
-            [['AB', '1945; 1946; 1966'], 'AB (1945; 1946; 1966)'],
-            [['AB', '1945, 1946'], 'AB (1945, 1946)'],
-            [['AB', '1945, 1946, 1949'], 'AB (1945, 1946, 1949)'],
-            [['AB', '1945-1946'], 'AB (1945-1946)'],
-            [['AB', '1945-6'], 'AB (1945-6)'],
-
-            [['AB', '1945'], 'AB [1945]'],
-            [['AB', '1945; 1946'], 'AB [1945; 1946]'],
-            [['AB', '1945; 1946; 1966'], 'AB [1945; 1946; 1966]'],
-            [['AB', '1945, 1946'], 'AB [1945, 1946]'],
-            [['AB', '1945, 1946, 1949'], 'AB [1945, 1946, 1949]'],
-            [['AB', '1945-1946'], 'AB [1945-1946]'],
-            [['AB', '1945-6'], 'AB [1945-6]'],
-            
-            [['AB', '1945'], 'AB ([1945])'],
-            [['AB', '1945; 1946'], 'AB ([1945; 1946])'],
-            [['AB', '1945; 1946; 1966'], 'AB ([1945; 1946; 1966])'],
-            [['AB', '1945, 1946'], 'AB ([1945, 1946])'],
-            [['AB', '1945, 1946, 1949'], 'AB ([1945, 1946, 1949])'],
-            [['AB', '1945-1946'], 'AB ([1945-1946])'],
-            [['AB', '1945-6'], 'AB ([1945-6])'],
-            
-            [['The Wind (Fire)', null], 'The Wind (Fire)'],
-        ];
+    public function testDupeData() {
+        $row = array_fill(0, 13, '');
+        $row[9] = 'Alberta Poetry Yearbook (1936); Alberta Poetry Yearbook (1938)';
+        
+        $this->importer->setCommit(true);
+        $this->importer->importRow($row);
+        $periodicals = $this->em->getRepository(Periodical::class)->findAll();
+        $this->assertEquals(1, count($periodicals));
     }
     
     // Crazy title cases.
-//    public function testCaseInsensitive() {
-//        $row = array_fill(0, 13, '');
-//        $row[9] = 'Alberta Poetry Yearbook (1936); ALBERTA POETRY YEARBOOK (1938)';
-//        
-//        $this->importer->setCommit(true);
-//        $this->importer->importRow($row);
-//        $periodicals = $this->em->getRepository(Periodical::class)->findAll();
-//        $this->assertEquals(1, count($periodicals));
-//    }
+    public function testCaseInsensitive() {
+        $row = array_fill(0, 13, '');
+        $row[9] = 'Alberta Poetry Yearbook (1936); ALBERTA POETRY YEARBOOK (1938)';
+        
+        $this->importer->setCommit(true);
+        $this->importer->importRow($row);
+        $periodicals = $this->em->getRepository(Periodical::class)->findAll();
+        $this->assertEquals(1, count($periodicals));
+    }
 
 }
