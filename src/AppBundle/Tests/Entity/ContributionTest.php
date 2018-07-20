776b224e4f5dd7ad01ac03d92ad58e0cc6d8ddff
Erik Hanson <ewhanson@sfu.ca>
2018-07-20 12:15:23 -0500

    Add getPublicationId test

---
+++ src/AppBundle/Tests/Entity/ContributionTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Tests\Entity;
+
+use AppBundle\Entity\Contribution;
+use AppBundle\Tests\DataFixtures\ORM\LoadPeriodical;
+use AppBundle\Tests\DataFixtures\ORM\LoadPerson;
+use AppBundle\Tests\DataFixtures\ORM\LoadRole;
+use Nines\UtilBundle\Tests\Util\BaseTestCase;
+
+class ContributionTest extends BaseTestCase {
+
+    protected function getFixtures() {
+        return [
+            LoadPeriodical::class,
+            LoadPerson::class,
+            LoadRole::class
+        ];
+    }
+
+    public function testGetPublicationId() {
+        $contribution = new Contribution();
+        $contribution->setPerson($this->getReference('person.1'));
+        $contribution->setRole($this->getReference('role.1'));
+        $contribution->setPublication($this->getReference('periodical.1'));
+
+        $this->assertEquals($this->getReference('periodical.1')->getId(), $contribution->getPublicationId());
+    }
+}
