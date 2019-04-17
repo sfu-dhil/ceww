7aa68bbc81a35bb83b1e44640354691a4cac2c4c
Michael Joyce <ubermichael@gmail.com>
2019-04-17 15:42:18 -0700

    and make the roles list sorted.

--- src/AppBundle/Controller/RoleController.php
+++ src/AppBundle/Controller/RoleController.php
@@ -30,7 +30,7 @@ class RoleController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $qb = $em->createQueryBuilder();
-        $qb->select('e')->from(Role::class, 'e')->orderBy('e.id', 'ASC');
+        $qb->select('e')->from(Role::class, 'e')->orderBy('e.label', 'ASC');
         $query = $qb->getQuery();
         $paginator = $this->get('knp_paginator');
         $roles = $paginator->paginate($query, $request->query->getint('page', 1), $this->getParameter('page_size'));
